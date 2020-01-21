54900f250728160b7bd2499f86a3fe6632b59544
Seokgyu Lee <dltjrrb0729@naver.com>
2020-01-21 21:32:20 +0900

    done

--- src/Movie.js
+++ src/Movie.js
@@ -18,7 +18,7 @@ function Movie({ year, title, summary, poster, genres }) {
             </li>
           ))}
         </ul>
-        <p className="movie__data__summary"> {summary}</p>
+        <p className="movie__data__summary"> {summary.slice(0, 140)}...</p>
       </div>
     </div>
   );
