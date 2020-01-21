4176d52747dee4b9490a62b430d557a7623ab173
Seokgyu Lee <dltjrrb0729@naver.com>
2020-01-21 21:42:47 +0900

    Updates

---
+++ precache-manifest.42dfab7c152881d3a2adee9d44e34a6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066aaa4f2d4f2989997866dad12026f7",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "f43edf47896d120617cb",
+    "url": "/movie_app_2020/static/css/main.1dac1b8c.chunk.css"
+  },
+  {
+    "revision": "07f2d2c5569d28c9abab",
+    "url": "/movie_app_2020/static/js/2.c0126add.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2020/static/js/2.c0126add.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f43edf47896d120617cb",
+    "url": "/movie_app_2020/static/js/main.09e2e596.chunk.js"
+  },
+  {
+    "revision": "90b45c5c50d92ea532b6",
+    "url": "/movie_app_2020/static/js/runtime-main.2c9a6a91.js"
+  }
+]);
